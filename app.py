5ff04e441b3b9e87384be4b9c2cbe77908b68cec
BartoszBury1 <91828936+BartoszBury1@users.noreply.github.com>
2022-05-08 11:03:03 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,11 +1,9 @@
-'''Modul Flask'''
+"""Module to import flask"""
 from flask import Flask
 app = Flask(__name__)
-
 @app.route("/")
 def index():
     """Funkcji lint"""
     return "<h1>Hello WSB! Greetings from Flask!</h1>"
-
 if __name__ == "__main__":
     app.run(debug=True)
